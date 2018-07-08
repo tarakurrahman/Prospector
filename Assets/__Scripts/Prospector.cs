8684abb3019fda61ec80201dbf0f51181c554a63
Tarakur Rahman <39719842+tarakurrahman@users.noreply.github.com>
2018-07-08 14:21:16 -0400

    Laid out the Mine Tableau
    Next step: Positioning cards in the tableau

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -9,9 +9,12 @@ public class Prospector : MonoBehaviour {
 
     [Header("Set in Inspector")]
     public TextAsset deckXML;
+    public TextAsset layoutXML;
 
     [Header("Set Dynamically")]
     public Deck deck;
+    public Layout layout;
+    public List<CardProspector> drawPile;
 
     void Awake() {
         S = this;
@@ -22,10 +25,25 @@ public class Prospector : MonoBehaviour {
         deck.InitDeck(deckXML.text);
         Deck.Shuffle(ref deck.cards);
 
-        Card c;
-        for (int cNum = 0; cNum < deck.cards.Count; cNum++) {
-            c = deck.cards[cNum];
-            c.transform.localPosition = new Vector3((cNum % 13) * 3, cNum / 13 * 4, 0);
+        /*   Card c;
+           for (int cNum = 0; cNum < deck.cards.Count; cNum++) {
+               c = deck.cards[cNum];
+               c.transform.localPosition = new Vector3((cNum % 13) * 3, cNum / 13 * 4, 0);
+           }
+           */
+        layout = GetComponent<Layout>();
+        layout.ReadLayout(layoutXML.text);
+
+        drawPile = ConvertListCardsToListCardProspectors(deck.cards);
+    }
+
+    List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD) {
+        List<CardProspector> lCP = new List<CardProspector>();
+        CardProspector tCP;
+        foreach (Card tCD in lCD) {
+            tCP = tCD as CardProspector;
+            lCP.Add(tCP);
         }
+        return (lCP);
     }
 }
