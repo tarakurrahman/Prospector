4fce4c8cb1dc2f090788b65e064d8eb7588dc12c
Tarakur Rahman <39719842+tarakurrahman@users.noreply.github.com>
2018-07-08 12:28:55 -0400

    Parsed information from deck XML
    - Assigned the sprites that become cards
    - Created Prefab GameObjects for sprites and cards
    - Next step: Build the cards in code

--- Assets/__Scripts/Deck.cs
+++ Assets/__Scripts/Deck.cs
@@ -4,8 +4,33 @@ using UnityEngine;
 
 public class Deck : MonoBehaviour {
 
+    [Header("Set in Inspector")]
+    // Suits
+    public Sprite suitClub;
+    public Sprite suitDiamond;
+    public Sprite suitHeart;
+    public Sprite suitSpade;
+
+    public Sprite[] faceSprites;
+    public Sprite[] rankSprites;
+
+    public Sprite cardBack;
+    public Sprite cardBackGold;
+    public Sprite cardFront;
+    public Sprite cardFrontGold;
+    
+    // Prefabs
+    public GameObject prefabCard;
+    public GameObject prefabSprite;
+
     [Header("Set Dynamically")]
     public PT_XMLReader xmlr;
+    public List<string> cardNames;
+    public List<Card> cards;
+    public List<Decorator> decorators;
+    public List<CardDefinition> cardDefs;
+    public Transform deckAnchor;
+    public Dictionary<string, Sprite> dictSuits;
 
     public void InitDeck(string deckXMLText) {
         ReadDeck(deckXMLText);
@@ -20,6 +45,59 @@ public class Deck : MonoBehaviour {
         s += " x=" + xmlr.xml["xml"][0]["decorator"][0].att("x");
         s += " y=" + xmlr.xml["xml"][0]["decorator"][0].att("y");
         s += " scale=" + xmlr.xml["xml"][0]["decorator"][0].att("scale");
-        print(s);
+        // print(s);
+
+        decorators = new List<Decorator>();
+
+        PT_XMLHashList xDecos = xmlr.xml["xml"][0]["decorator"];
+        Decorator deco;
+        for (int i = 0; i < xDecos.Count; i++) {
+            deco = new Decorator();
+
+            deco.type = xDecos[i].att("type");
+
+            deco.flip = (xDecos[i].att("flip") == "1");
+
+            deco.scale = float.Parse(xDecos[i].att("scale"));
+
+            deco.loc.x = float.Parse(xDecos[i].att("x"));
+            deco.loc.y = float.Parse(xDecos[i].att("y"));
+            deco.loc.z = float.Parse(xDecos[i].att("z"));
+
+            decorators.Add(deco);
+        }
+
+        cardDefs = new List<CardDefinition>();
+
+        PT_XMLHashList xCardDefs = xmlr.xml["xml"][0]["card"];
+        for (int i = 0; i < xCardDefs.Count; i++) {
+
+            CardDefinition cDef = new CardDefinition();
+
+            cDef.rank = int.Parse(xCardDefs[i].att("rank"));
+
+            PT_XMLHashList xPips = xCardDefs[i]["pip"];
+            if (xPips != null) {
+                for (int j = 0; j < xPips.Count; j++) {
+
+                    deco = new Decorator();
+
+                    deco.type = "pip";
+                    deco.flip = (xPips[j].att("flip") == "1");
+                    deco.loc.x = float.Parse(xPips[j].att("x"));
+                    deco.loc.y = float.Parse(xPips[j].att("y"));
+                    deco.loc.z = float.Parse(xPips[j].att("z"));
+                    if (xPips[j].HasAtt("scale")) {
+                        deco.scale = float.Parse(xPips[j].att("scale"));
+                    }
+                    cDef.pips.Add(deco);
+                }
+            }
+
+            if (xCardDefs[i].HasAtt("face")) {
+                cDef.face = xCardDefs[i].att("face");
+            }
+            cardDefs.Add(cDef);
+        }
     }
 }
